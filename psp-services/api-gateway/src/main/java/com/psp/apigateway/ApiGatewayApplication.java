43f12e0b53600f1ad3b566a9b543cde4958df93b
AleksandarSekulic1 <149141670+AleksandarSekulic1@users.noreply.github.com>
2026-01-01 23:33:53 +0100

    Fixed API Gateway versions and routing configuration

--- psp-services/api-gateway/src/main/java/com/psp/apigateway/ApiGatewayApplication.java
+++ psp-services/api-gateway/src/main/java/com/psp/apigateway/ApiGatewayApplication.java
@@ -2,6 +2,9 @@ package com.psp.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -10,4 +13,13 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+	// OVDE DEFINISEMO RUTU PROGRAMSKI (Umesto u YAML-u)
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+			.route("core-service", r -> r.path("/core/**") // Ako putanja krece sa /core...
+				.filters(f -> f.stripPrefix(1))            // ...obrisi "/core"...
+				.uri("http://localhost:8081"))             // ...i posalji na port 8081.
+			.build();
+	}
 }
