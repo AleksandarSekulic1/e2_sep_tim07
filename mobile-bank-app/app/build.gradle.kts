fdac25dc45c038ab2ef2857e3490ba166fb36d9e
AleksandarSekulic1 <149141670+AleksandarSekulic1@users.noreply.github.com>
2026-01-03 23:19:52 +0100

    feat: implement mobile mBanking simulator and QR payment flow confirmation

--- mobile-bank-app/app/build.gradle.kts
+++ mobile-bank-app/app/build.gradle.kts
@@ -33,16 +33,26 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    // DODAJEMO VIEW BINDING ZA LAKŠI RAD SA DUGMIĆIMA
+    viewBinding {
+        enable = true
+    }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    // DODATE BIBLIOTEKE
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0") // Za skeniranje QR
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")       // Za mrežne pozive
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0") // Za JSON
+
     testImplementation(libs.junit)
+    androidTestImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
